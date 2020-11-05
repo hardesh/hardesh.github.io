81cc4b2299d79bd654677d73ed87e3a890edffc6
Harshal Deshpande <hardesh1deshpande@gmail.com>
2020-11-05 12:52:54 +0530

    fixed issues

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,9 @@ fi
 
 # echo "Deleting old publication"
 rm -rf public
-# mkdir public
-# git worktree prune
-# rm -rf .git/worktrees/public/
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
 git worktree add -B gh-pages public
